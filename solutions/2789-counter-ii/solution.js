6e6d79842da09d312808160843d24d295dc692d3
sahil mor <135847772+sahilmor@users.noreply.github.com>
2024-08-24 16:08:33 +0000

    [LeetCode Sync] Runtime - 58 ms (21.59%), Memory - 51.8 MB (100.00%)

---
+++ solutions/2789-counter-ii/solution.js
@@ -0,0 +1,28 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    let i = init;
+    return{
+        increment: function(){
+            i++;
+            return i;
+        },
+        reset: function(){
+            i = init;
+            return i;
+        },
+        decrement: function(){
+            i--;
+            return i;
+        }
+    }     
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
