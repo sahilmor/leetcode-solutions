b94745e682b7511d149ef40cb8bfae157f7d0b1b
sahil mor <135847772+sahilmor@users.noreply.github.com>
2024-09-19 18:08:53 +0000

    Sync LeetCode submission - Filter Elements from Array (javascript)

---
+++ solutions/problems/filter_elements_from_array/solution.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var filter = function(arr, fn) {
+    let newArr = [];
+
+  arr.forEach((value, index) => {
+    if (fn(value, index)) {
+      newArr.push(value);
+    }
+  });
+  return newArr;
+};
