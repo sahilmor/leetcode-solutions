cdbbed2532d4f66b1ec1cb22d11d78fa057b05ad
sahil mor <135847772+sahilmor@users.noreply.github.com>
2025-04-04 17:05:37 +0000

    Sync LeetCode submission - Two Sum (cpp)

--- solutions/problems/two_sum/solution.cpp
+++ solutions/problems/two_sum/solution.cpp
@@ -1,17 +1,13 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        map<int, int> mpp;
         for(int i = 0; i < nums.size(); i++){
-            int value = target-nums[i];
-            auto result = mpp.find(value);
-            if(result != mpp.end()){
-                return {mpp[value], i};
-            }
-            else{
-                mpp.insert({nums[i],i});
+            for(int j = i+1; j < nums.size(); j++){
+                if((nums[i] + nums[j]) == target){
+                    return{i, j};
+                }
             }
         }
-        return {};
+            return {};
     }
 };
