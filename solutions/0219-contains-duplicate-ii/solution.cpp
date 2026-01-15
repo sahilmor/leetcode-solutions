48743e793a289fb764909d856a4db2c4d539dd5f
sahil mor <135847772+sahilmor@users.noreply.github.com>
2026-01-15 20:07:11 +0000

    [LeetCode Sync] Runtime - 1803 ms (5.05%), Memory - 74 MB (96.64%)

---
+++ solutions/0219-contains-duplicate-ii/solution.cpp
@@ -0,0 +1,14 @@
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        for(int i = 0; i < nums.size(); i++){
+            for(int j = i + 1; j <= i + k && j < nums.size(); j++){
+                if(nums[i] == nums[j]){
+                    return true;
+                }
+            }
+        }        return false;
+    }
+};
